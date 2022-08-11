3c3f2d53601869bd9f6bd9821d8ed0ba9861cc7d
mohsen madi <mohsen@backbase.com>
2022-08-10 22:29:54 -0400

    table UI done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Transaction {
+  id: number;
+  item: string;
+  cost: number;
+  orders?: number | undefined;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'summit-comp-store';
+  displayedColumns: string[] = ['item', 'cost', 'orders'];
+  transactions: Transaction[] = [
+    {id: 10, item: 'Beach ball', cost: 14},
+    {id: 20, item: 'Towel', cost: 5},
+    {id: 30, item: 'Frisbee', cost: 2},
+    {id: 40, item: 'Sunscreen', cost: 4},
+    {id: 50, item: 'Cooler', cost: 25},
+    {id: 60, item: 'Swim suit', cost: 15},
+  ];
+
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  }
+
+  getOrders(orders: any, element:any) {
+    console.log(orders, element)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from "@angular/material/table";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
